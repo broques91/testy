7034e53fbf11893fedc049dfd6cae20bdb0738b9
Bastien Roques <bastien.roques971@gmail.com>
2021-06-24 16:11:45 +0200

    first commit

---
+++ posts/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
+
+
+class PostTests(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text="just a test")
+
+    def test_text_context(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f"{post.text}"
+        self.assertEquals(expected_object_name, "just a test")
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('posts'))
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "just a test")
+        self.assertTemplateUsed(response, "posts.html")
+
+
